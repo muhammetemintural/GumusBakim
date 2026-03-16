1c509539ec6a60a7109b2dfd37d61c19c8d39d21
muhammet emin tural <muhammetemintural@gmail.com>
2026-03-16 23:13:29 +0300

    html sayfaları oluşturulmaya başlandı ve bağlantılar yapılıyor

--- hastalar/admin.py
+++ hastalar/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Hasta
 
-admin.site.register(Hasta)
+@admin.register(Hasta)
+class HastaAdmin(admin.ModelAdmin):
+    list_display = ('ad', 'soyad', 'tc_kimlik_no', 'telefon', 'kan_grubu') # Ana listede görünecek sütunlar
+    search_fields = ('ad', 'soyad', 'tc_kimlik_no') # Arama çubuğu ekler
--- hesaplar/views.py
+++ hesaplar/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
-# Create your views here.
+# 1. Ana Karşılama Sayfası
+def ana_sayfa(request):
+    return render(request, 'index.html')
+
+# 2. Personel Giriş (Login) İşlemleri
+def personel_giris(request):
+    # Eğer butona basılmışsa (POST isteği geldiyse)
+    if request.method == 'POST':
+        kullanici = request.POST.get('username')
+        sifre = request.POST.get('password')
+
+        # Güvenlik Şefi: Veri tabanındaki şifreyle eşleşiyor mu kontrol et
+        user = authenticate(request, username=kullanici, password=sifre)
+
+        if user is not None:
+            # Şifre doğruysa kapıyı aç ve oturum başlat
+            login(request, user)
+            return redirect('dashboard') # İçeri al
+        else:
+            # Yanlışsa ekrana kırmızı hata mesajı gönder
+            messages.error(request, 'Kullanıcı adı veya şifre hatalı. Lütfen kontrol ediniz.')
+
+    # Butona basılmadıysa sadece formu göster
+    return render(request, 'login.html')
+
+# 3. İçerisi (Giriş yaptıktan sonra açılacak olan ana ekran)
+def dashboard(request):
+    return render(request, 'dashboard.html')
